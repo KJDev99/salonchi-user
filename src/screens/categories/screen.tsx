ed493687cc2ffccc34c21f244bedccf7ba9d1191
jamshidqayimov <jamshidqayimov0399@gmail.com>
2024-09-19 12:04:42 +0500

    push image

--- src/screens/categories/screen.tsx
+++ src/screens/categories/screen.tsx
@@ -13,28 +13,33 @@ import { Pagination } from "@/components/pagination";
 const CategoriesScreen = () => {
   const router = useRouter();
   const categoryId = router.query.slug as string;
-  const { productList,isLoading,setPage,activePage } = useProducts();
-  const { t } = useTranslation("common");;
+  const { productList, isLoading, setPage, activePage } = useProducts();
+  const { t } = useTranslation("common");
 
   return (
     <Sidebar categoryId={categoryId}>
-      <Title>{t('home.title')}</Title>
+      <Title>{t("home.title")}</Title>
       <Grid gutter={16}>
-        {!isLoading && productList?.data?.map((item: IProduct) => (
-          <Grid.Col
-            span={6}
-            lg={4}
-            md={3}
-            xs={4}
-            // sm={4}
-            key={item.id}
-            style={{ background: "red !important" }}
-          >
-            <CustomCard id={item.id} item={item} />
-          </Grid.Col>
-        ))}
+        {!isLoading &&
+          productList?.data?.map((item: IProduct) => (
+            <Grid.Col
+              span={6}
+              lg={4}
+              md={3}
+              xs={4}
+              // sm={4}
+              key={item.id}
+              style={{ background: "red !important" }}
+            >
+              <CustomCard id={item.id} item={item} />
+            </Grid.Col>
+          ))}
       </Grid>
-     <Pagination productCount={productList.count} activePage={activePage} setPage={(e) => setPage(e)} />
+      <Pagination
+        productCount={productList.count}
+        activePage={activePage}
+        setPage={(e) => setPage(e)}
+      />
     </Sidebar>
   );
 };
