0722a281798e058344af85109baa5443274c9852
Omonov <algoritm1202@gmail.com>
2025-02-24 12:37:24 +0500

    push

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { createGetInitialProps } from "@mantine/next";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 const getInitialProps = createGetInitialProps();
 
@@ -8,11 +9,43 @@ export default class _Document extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+          {/* Google Analytics */}
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-N2HP8DYEXY"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-N2HP8DYEXY');
+            `}
+          </Script>
+
+          {/* Yandex Metrika */}
+          <Script id="yandex-metrika" strategy="afterInteractive">
+            {`
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t), a=e.getElementsByTagName(t)[0], k.async=1, k.src=r, a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(100012838, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+              });
+            `}
+          </Script>
+
+          {/* Facebook Pixel */}
+          <Script id="facebook-pixel" strategy="afterInteractive">
+            {`
               !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -23,21 +56,30 @@ export default class _Document extends Document {
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '529375946403229');
               fbq('track', 'PageView');
-            `,
-            }}
-          />
+            `}
+          </Script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
           <noscript>
+            {/* Facebook Pixel noscript */}
             <img
               height="1"
               width="1"
               style={{ display: "none" }}
               src="https://www.facebook.com/tr?id=529375946403229&ev=PageView&noscript=1"
+              alt=""
             />
+            {/* Yandex Metrika noscript */}
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/100012838"
+                style={{ position: "absolute", left: "-9999px" }}
+                alt=""
+              />
+            </div>
           </noscript>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
         </body>
       </Html>
     );
